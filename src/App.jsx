0bc53496ece4aa79bc2c447cc3031d8e5d2ff560
heavydejo <heavydejo1@gmail.com>
2026-05-06 15:12:03 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{padding:'20px',fontFamily:'Arial'}}>
+      <h1>PETEX Porudžbenice</h1>
+      <p>Test verzija aplikacije radi 🎉</p>
+    </div>
+  )
+}
